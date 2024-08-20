ecaf3b6c54b8b0f55ba433fcb49b36e38cb3d966
Victor F Ramos <victorramosfernando7299@gmail.com>
2024-08-20 00:30:14 -0500

    added menu-es page and some minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
   extend: {
       colors:{
@@ -11,6 +11,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+        require('flowbite/plugin')
+    ]
 }
 
